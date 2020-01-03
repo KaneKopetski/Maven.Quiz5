9b29ac27932bac76a68994803b0dd2321f0da11a
KaneKopetski <KaneKopetski@users.noreply.github.com>
2020-01-03 09:28:44 -0500

    Student done

--- src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
@@ -4,17 +4,25 @@ package rocks.zipcode.io.quiz4.collections;
  * @author leon on 11/12/2018.
  */
 public class Student {
+    private Integer id;
+    private Double totalStudyTime;
+
     public Student() {
-        this(null);
+        this(0);
     }
 
     public Student(Integer id) {
+        this.id = id;
+        totalStudyTime = 0D;
     }
 
     public void learn(Double amountOfHours) {
+        if (amountOfHours > 0 ) {
+            totalStudyTime += amountOfHours;
+        }
     }
 
     public Double getTotalStudyTime() {
-        return null;
+        return totalStudyTime;
     }
 }
