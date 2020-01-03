15ee441e98690c44333795708c6d15d540f8cec0
KaneKopetski <KaneKopetski@users.noreply.github.com>
2020-01-03 10:00:59 -0500

    Complete fundamentals

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,34 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
+
     public static String[] getAllPalindromes(String string) {
-        return null;
+        String[] substrings = StringEvaluator.getAllSubstrings(string);
+        ArrayList<String> palindromes = new ArrayList<>();
+
+        for (String substringToEval: substrings) {
+            if (isPalindrome(substringToEval)) {
+                palindromes.add(substringToEval);
+            }
+        }
+        return palindromes.toArray(new String[0]);
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        return string.equals(reverseString(string));
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(string);
+        sb = sb.reverse();
+
+        return sb.toString();
     }
 }
