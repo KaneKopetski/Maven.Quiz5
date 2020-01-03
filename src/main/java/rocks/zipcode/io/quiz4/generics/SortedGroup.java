b515f77ecbcce3611a2028c8eb8de45211c605c8
KaneKopetski <KaneKopetski@users.noreply.github.com>
2020-01-03 10:20:40 -0500

    Easy Stack and SortedGroup done

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -3,16 +3,24 @@ package rocks.zipcode.io.quiz4.generics;
 /**
  * @author leon on 18/12/2018.
  */
-public class SortedGroup<_> extends Group<_> {
+public class SortedGroup<T extends Comparable> extends Group<T> {
+
     @Override
-    public void insert(_ value) {
+    public void insert(T value) {
+        this.group.add(value);
+        this.group.sort((object1, object2) -> {
+            if (object1 instanceof Comparable) {
+            return (object1).compareTo(object2);
+        }
+        else return 0;});
     }
 
     @Override
-    public void delete(_ value) {
+    public void delete(T value) {
+        this.group.remove(value);
     }
 
-    public Integer indexOf(_ value) {
-        return null;
+    public Integer indexOf(T value) {
+        return super.group.indexOf(value);
     }
 }
